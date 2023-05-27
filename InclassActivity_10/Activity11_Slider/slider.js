651d93afdb56f0da4b951d4bdc7e0652ff405c20
Bpasazade <b.pasazade@outlook.com>
2023-05-27 15:15:14 +0300

    ICA_10 update

--- InclassActivity_10/Activity11_Slider/slider.js
+++ InclassActivity_10/Activity11_Slider/slider.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-        	$("#slider").bxSlider({
-			  auto: true,
-			  minSlides: 1,
-			  maxSlides: 1,
-			  slideWidth: 250,
-			  slideMargin: 10,
-			  randomStart: true,
-			  moveSlides: 1,
-			  pager: true,
-			  pagerType: 'short',
-			  captions: true,
-			});
-    	});
+    $("#slider").bxSlider({
+		auto: true,
+		minSlides: 1,
+		maxSlides: 1,
+		slideWidth: 250,
+		slideMargin: 10,
+		randomStart: true,
+		moveSlides: 1,
+		pager: true,
+		pagerType: 'short',
+		captions: true,
+	});
+});
